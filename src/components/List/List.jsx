fa6872c0d9b7c816e726b56761e191a6a752d938
mariaclaramaciel <mariaclaramaciel@icloud.com>
2021-12-06 19:32:55 -0300

    finaliza testes

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -26,6 +26,16 @@ const List = () => {
     setList(tasksFiltered)
   }
 
+  function handleToggTaskCompletion(id) {
+    const newTasks = list.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task)
+
+    setList(newTasks)
+  
+  }
+
   return(
     <>
      <section className='list'>
@@ -58,7 +68,7 @@ const List = () => {
                   <input 
                     type="checkbox"
                     checked={task.isComplete}
-                    onClick={() => {}}
+                    onClick={() => handleToggTaskCompletion(task.id)}
                     readOnly
                   />
                   <span className="checkmark"></span>
